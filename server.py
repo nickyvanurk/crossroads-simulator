448fd57d3e7aa9852af812b86702ea1c86848359
Nicky van Urk <nickyvanurk@gmx.com>
2020-03-29 23:30:29 +0200

    Refactor traffic light data

--- server.py
+++ server.py
@@ -5,7 +5,143 @@ import websockets
 import json
 from enum import Enum
 
-traffic_light_ids = ['A1','A2','A3','A4','AB1','AB2','B1','B2','B3','B4','B5','BB1','C1','C2','C3','D1','D2','D3','E1','E2','EV1','EV2','EV3','EV4','FV1','FV2','FV3','FV4','FF1','FF2','GV1','GV2','GV3','GV4','GF1','GF2']
+# traffic_light_ids = ['A1','A2','A3','A4','AB1','AB2','B1','B2','B3','B4','B5','BB1','C1','C2','C3','D1','D2','D3','E1','E2','EV1','EV2','EV3','EV4','FV1','FV2','FV3','FV4','FF1','FF2','GV1','GV2','GV3','GV4','GF1','GF2']
+# traffic_light_groups = ['A2','A3','A4','B2','B3','B4',   'C2','C3','D2','D3',   'A1','E1','E2','EV1','EV2','EV3','EV4',    'B1','FV1','FV2','FV3','FV4','FF1','FF2','GV1','GV2','GV3','GV4','GF1','GF2',    'C1','E1','E2','EV1','EV2','EV3','EV4','FV1','FV2','FV3','FV4','FF1','FF2',    'D1','GV1','GV2','GV3','GV4','GF1','GF2']
+
+traffic_light_data = {
+  'A1': {
+    'origin': 'S',
+    'destination': 'W'
+  },
+  'A2': {
+    'origin': 'S',
+    'destination': 'N'
+  },
+  'A3': {
+    'origin': 'S',
+    'destination': 'N'
+  },
+  'A4': {
+    'origin': 'S',
+    'destination': 'E'
+  },
+  'AB1': {
+    'origin': 'S',
+    'destination': 'N'
+  },
+  'AB2': {
+    'origin': 'S',
+    'destination': 'E'
+  },
+  'B1': {
+    'origin': 'N',
+    'destination': 'E'
+  },
+  'B2': {
+    'origin': 'N',
+    'destination': 'S'
+  },
+  'B3': {
+    'origin': 'N',
+    'destination': 'S'
+  },
+  'B4': {
+    'origin': 'N',
+    'destination': 'W'
+  },
+  'B5': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'BB1': {
+    'origin': 'N',
+    'destination': 'S'
+  },
+  'C1': {
+    'origin': 'E',
+    'destination': 'S'
+  },
+  'C2': {
+    'origin': 'E',
+    'destination': 'W'
+  },
+  'D3': {
+    'origin': 'W',
+    'destination': 'S'
+  },
+  'E1': {
+    'origin': 'N',
+    'destination': 'N'
+  },
+  'E2': {
+    'origin': 'N',
+    'destination': 'N'
+  },
+  'EV1': {
+    'origin': 'N',
+    'destination': 'N'
+  },
+  'EV2': {
+    'origin': 'N',
+    'destination': 'N'
+  },
+  'EV3': {
+    'origin': 'N',
+    'destination': 'N'
+  },
+  'EV4': {
+    'origin': 'N',
+    'destination': 'N'
+  },
+  'FV1': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'FV2': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'FV3': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'FV4': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'FF1': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'FF2': {
+    'origin': 'W',
+    'destination': 'W'
+  },
+  'GV1': {
+    'origin': 'S',
+    'destination': 'S'
+  },
+  'GV2': {
+    'origin': 'S',
+    'destination': 'S'
+  },
+  'GV3': {
+    'origin': 'S',
+    'destination': 'S'
+  },
+  'GV4': {
+    'origin': 'S',
+    'destination': 'S'
+  },
+  'GF1': {
+    'origin': 'S',
+    'destination': 'S'
+  },
+  'GF2': {
+    'origin': 'S',
+    'destination': 'S'
+  }
+}
 
 
 class Color(Enum):
@@ -61,12 +197,12 @@ class CycleTrafficLight:
 class World:
   """A world containg all traffic lights logic"""
 
-  def __init__(self, traffic_light_ids):
+  def __init__(self, traffic_light_data):
     self.traffic_lights = { }
-    self.generate_traffic_lights(traffic_light_ids)
+    self.generate_traffic_lights(traffic_light_data)
 
-  def generate_traffic_lights(self, traffic_light_ids):
-    for id in traffic_light_ids:
+  def generate_traffic_lights(self, traffic_light_data):
+    for id, cardinal_directions in traffic_light_data.items():
       traffic_light_type = id[1]    
 
       if traffic_light_type == 'B':
@@ -88,7 +224,7 @@ class World:
     
   
 async def index(websocket, path):
-  world = World(traffic_light_ids)
+  world = World(traffic_light_data)
 
   async for message in websocket:
     data = json.loads(message)
